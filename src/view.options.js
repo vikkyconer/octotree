dafae811ed6a9b57c0a62c01ffa1535f5016a6e3
Vikas Chandra <vikkycorner@gmail.com>
2020-08-08 18:19:44 +0530

    add redirect url for github oauth

--- src/view.options.js
+++ src/view.options.js
@@ -1,20 +1,24 @@
 class OptionsView {
   constructor($dom, adapter) {
     this.adapter = adapter;
-    this.$toggler = $dom.find('.octotree-settings').click(this.toggle.bind(this));
-    this.$view = $dom.find('.octotree-settings-view').submit((event) => {
+    this.$toggler = $dom
+      .find(".octotree-settings")
+      .click(this.toggle.bind(this));
+    this.$view = $dom.find(".octotree-settings-view").submit((event) => {
       event.preventDefault();
       this.toggle(false);
     });
 
-    $dom.find('.octotree-github-oauth').click((event) => {
+    $dom.find(".octotree-github-oauth").click((event) => {
       event.preventDefault();
-      console.log('octotree-github-oauth')
-      // window.location.href = platform.getOAuthLink()
-      alert(`We are working on it`)
-    })
+      console.log("octotree-github-oauth");
+      window.location.href =
+        "https://github.com/login/oauth/authorize?scope=user:email&client_id=cfa7d8c85ec9fa22261b";
+    });
 
-    this.$view.find('a.octotree-create-token').attr('href', this.adapter.getCreateTokenUrl());
+    this.$view
+      .find("a.octotree-create-token")
+      .attr("href", this.adapter.getCreateTokenUrl());
 
     this.loadElements();
 
@@ -30,7 +34,7 @@ class OptionsView {
    * elements, so that they can be loaded and saved.
    */
   loadElements() {
-    this.elements = this.$view.find('[data-store]').toArray();
+    this.elements = this.$view.find("[data-store]").toArray();
   }
 
   /**
@@ -38,13 +42,13 @@ class OptionsView {
    */
   toggle(visibility) {
     if (visibility !== undefined) {
-      if (this.$view.hasClass('current') === visibility) return;
+      if (this.$view.hasClass("current") === visibility) return;
       return this.toggle();
     }
 
-    if (this.$toggler.hasClass('selected')) {
+    if (this.$toggler.hasClass("selected")) {
       this._save();
-      this.$toggler.removeClass('selected');
+      this.$toggler.removeClass("selected");
       $(this).trigger(EVENT.VIEW_CLOSE);
     } else {
       this._load();
@@ -54,17 +58,17 @@ class OptionsView {
   _load() {
     this._eachOption(
       ($elm, key, value, cb) => {
-        if ($elm.is(':checkbox')) {
-          $elm.prop('checked', value);
-        } else if ($elm.is(':radio')) {
-          $elm.prop('checked', $elm.val() === value);
+        if ($elm.is(":checkbox")) {
+          $elm.prop("checked", value);
+        } else if ($elm.is(":radio")) {
+          $elm.prop("checked", $elm.val() === value);
         } else {
           $elm.val(value);
         }
         cb();
       },
       () => {
-        this.$toggler.addClass('selected');
+        this.$toggler.addClass("selected");
         $(this).trigger(EVENT.VIEW_READY);
       }
     );
@@ -74,10 +78,12 @@ class OptionsView {
     const changes = {};
     this._eachOption(
       async ($elm, key, value, cb) => {
-        if ($elm.is(':radio') && !$elm.is(':checked')) {
+        if ($elm.is(":radio") && !$elm.is(":checked")) {
           return cb();
         }
-        const newValue = $elm.is(':checkbox') ? $elm.is(':checked') : $elm.val();
+        const newValue = $elm.is(":checkbox")
+          ? $elm.is(":checked")
+          : $elm.val();
         if (value === newValue) return cb();
         changes[key] = [value, newValue];
         await extStore.set(key, newValue);
@@ -92,7 +98,7 @@ class OptionsView {
       this.elements,
       async (elm, cb) => {
         const $elm = $(elm);
-        const key = STORE[$elm.data('store')];
+        const key = STORE[$elm.data("store")];
         const value = await extStore.get(key);
 
         processFn($elm, key, value, () => cb());
